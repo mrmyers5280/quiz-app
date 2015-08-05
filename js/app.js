4221125a6d867579a786e19eb227f317a7273d21
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-08-05 15:01:48 -0600

    With app.js, add for-in loop to console.log properties of an object - for testing

--- js/app.js
+++ js/app.js
@@ -6,10 +6,9 @@ function Question(ask, answers, correct) {
 }
 
 Question.prototype.sayQuestion = function() {
-    return "Question 1: " + this.ask + "answers: " + this.answers + "correct: " + this.correct;
+    return "Question 1: " + this.ask + " answers: " + this.answers + " correct: " + this.correct;
 }
 var question1 = new Question("In Star Wars, A New Hope, in which spaceport was the cantina?", ["Mos Eisley", "Los Eisley", "Tattoine", "Anchorhead"], 0);
-console.log(question1.sayQuestion());
 
 var question2 = new Question("The movies Tron: Legacy (2010) and Oblivion (2013) have which person in common?", ["Actor, Tom Cruise", "Actor, Jeff Bridges", "Director, Joseph Kosinski", "Soundtrack Artist, M83"], 2);
 
@@ -18,6 +17,12 @@ var question3 = new Question("In The Godfather II, to which city does Don Corleo
 var question4 = new Question("In Lord of the Rings: Fellowship of the Ring, what birthday is Bilbo celebrating?", ["115th", "111th", "100th", "90th"], 1);
 
 var question5 = new Question("In John Carter, to which planet in our solar system does the name 'Barsoom' refer?", ["Venus", "Earth", "Mars", "Jupiter"], 2);
+
+console.log(question1.sayQuestion());
+for (var i in question1) {
+    console.log(question1[i]);
+}
+
 
 // var question1 = {
 //     ask: "In Star Wars, A New Hope, in which spaceport was the cantina?",
